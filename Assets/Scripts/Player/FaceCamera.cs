8b5ea696a5714981dc19a73e6e0ced14fdb199fb
nefrob <25070989+nefrob@users.noreply.github.com>
2021-03-08 20:37:28 -0700

    Small fixes (#5)
    * Layers, healthbar camera look
    
    * Building class and prefab
    
    * Compute velocity for given angle particle trajectory to reach target position
    
    * Clamp camera move and zoom

---
+++ Assets/Scripts/Player/FaceCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    [SerializeField] private Transform camTransform = null;
+
+    private void Start()
+    {
+        if (camTransform == null)
+        {
+            camTransform = Camera.main.transform;
+        }
+    }
+
+    void LateUpdate()
+    {
+        transform.LookAt(
+            transform.position + camTransform.rotation * Vector3.forward,
+            camTransform.rotation * Vector3.up);
+    }
+}
